84ad69ded8cb9263259d54ec3e5c458d49f87f5c
Andrew Biggs (adb) <adb@cisco.com>
2016-02-21 15:15:35 -0700

    make 20qs work at a basic level

--- javascript/twenty_questions.js
+++ javascript/twenty_questions.js
@@ -1,7 +1,46 @@
-function processYes() {
-    document.getElementById("question").innerHTML = "you said yes";
+
+var rootQuestion = null;
+var currentQuestion = null;
+
+function Question(qtext, yes, no) {
+    this.qtext = qtext;
+    this.yes = yes;
+    this.no = no;
 }
 
-function processNo() {
-    document.getElementById("question").innerHTML = "you said no";
-}
+Question.prototype.ask = function () {
+    currentQuestion = this;
+    document.getElementById("question").innerHTML = currentQuestion.qtext;
+};
+
+Question.prototype.declareVictory = function () {
+    document.getElementById("question").innerHTML = "Yay, I won!";
+};
+
+Question.prototype.declareDefeat = function () {
+    document.getElementById("question").innerHTML = "Rats, I lost.";
+    document.getElementById("learnForm").removeAttribute("hidden");
+};
+
+Question.prototype.respondToYes = function () {
+    (this.yes != null) ? this.yes.ask() : this.declareVictory();
+};
+
+Question.prototype.respondToNo = function () {
+    (this.no != null) ? this.no.ask() : this.declareDefeat();
+};
+
+Question.prototype.learn = function () {
+    this.no = new Question(this.qtext, this.yes, this.no);
+    this.yes = new Question("Is it a " + document.getElementById("learnThing").value + "?", null, null);
+    this.qtext = document.getElementById("learnQuestion").value;
+    document.getElementById("learnForm").setAttribute("hidden", "true");
+    rootQuestion.ask();
+};
+
+function initialize() {
+    rootQuestion = new Question("Is it a monkey?", null, null);
+    document.getElementById("questionForm").removeAttribute("hidden");
+    rootQuestion.ask();
+};
+
